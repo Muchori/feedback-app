726cc283b64d4e16b4e04e38b63fe226f535099d
Muchori Joseph <muchorijoseph97@gmail.com>
2022-05-20 13:47:31 +0300

    Adding Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types'
+
+function Header({ text, bgColor, textColor }) {
+  // inline styles
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  )
+}
+
+/**
+ * Props of the header, properties, adding styles
+ */
+Header.defaultProps = {
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)', // styling of the gader
+  textColor: '#ff6a95',
+}
+
+/**
+ * PropTypes for checking type of the text
+ */
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+}
+
+export default Header
